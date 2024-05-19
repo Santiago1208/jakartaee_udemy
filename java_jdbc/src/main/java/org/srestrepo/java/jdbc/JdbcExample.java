5e75eb7d0c59ec72c6fd8a5312fc89ff9507ab39
Santiago Restrepo <santiago.restrepop@perficient.com>
2024-05-19 18:34:08 -0500

    Improve connection managing with try/catch with resources

--- java_jdbc/src/main/java/org/srestrepo/java/jdbc/JdbcExample.java
+++ java_jdbc/src/main/java/org/srestrepo/java/jdbc/JdbcExample.java
@@ -10,31 +10,17 @@ public class JdbcExample {
 
     public static void main(String[] args) {
         String url = "jdbc:mysql://localhost:3306/jakarta_course";
-        String username = "SANTIAGO2";
+        String username = "SANTIAGO";
         String password = "password";
 
-        Connection connection = null;
-        Statement statement = null;
-        ResultSet result = null;
-
-        try {
-            connection = DriverManager.getConnection(url, username, password);
-            statement = connection.createStatement();
-            result = statement.executeQuery("SELECT * FROM PRODUCTS");
-
+        try (Connection connection = DriverManager.getConnection(url, username, password);
+        Statement statement = connection.createStatement();
+        ResultSet result = statement.executeQuery("SELECT * FROM PRODUCTS")) {
             while (result.next()) {
                 System.out.println(result.getString("name"));
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
-        } finally {
-            try {
-                result.close();
-                statement.close();
-                connection.close();
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
         }
     }
 }
