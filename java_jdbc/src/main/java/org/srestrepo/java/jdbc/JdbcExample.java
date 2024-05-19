be01d8fea2c9a6294c40f820cf36d5b0c8f03aa7
Santiago Restrepo <santiago.restrepop@perficient.com>
2024-05-19 18:26:01 -0500

    Improve error handling
    Added a nested try/catch to handle the case when the database name, the username, the password or the table name is incorrect so the resources will still be going to be closed.

--- java_jdbc/src/main/java/org/srestrepo/java/jdbc/JdbcExample.java
+++ java_jdbc/src/main/java/org/srestrepo/java/jdbc/JdbcExample.java
@@ -10,21 +10,31 @@ public class JdbcExample {
 
     public static void main(String[] args) {
         String url = "jdbc:mysql://localhost:3306/jakarta_course";
-        String username = "SANTIAGO";
+        String username = "SANTIAGO2";
         String password = "password";
+
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet result = null;
+
         try {
-            Connection connection = DriverManager.getConnection(url, username, password);
-            Statement statement = connection.createStatement();
-            ResultSet result = statement.executeQuery("SELECT * FROM PRODUCTS");
+            connection = DriverManager.getConnection(url, username, password);
+            statement = connection.createStatement();
+            result = statement.executeQuery("SELECT * FROM PRODUCTS");
 
             while (result.next()) {
                 System.out.println(result.getString("name"));
             }
-            result.close();
-            statement.close();
-            connection.close();
         } catch (SQLException e) {
             throw new RuntimeException(e);
+        } finally {
+            try {
+                result.close();
+                statement.close();
+                connection.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
