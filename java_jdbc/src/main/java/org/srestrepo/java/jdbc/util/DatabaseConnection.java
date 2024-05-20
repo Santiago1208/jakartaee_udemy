5f3892d12efee72321b3f51b4a663643eefaf571
Santiago Restrepo <santiago.restrepop@perficient.com>
2024-05-19 23:28:46 -0500

    Update DatabaseConnection to return new connection if the current one is closed

--- java_jdbc/src/main/java/org/srestrepo/java/jdbc/util/DatabaseConnection.java
+++ java_jdbc/src/main/java/org/srestrepo/java/jdbc/util/DatabaseConnection.java
@@ -5,14 +5,16 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static String url = "jdbc:mysql://localhost:3306/jakarta_course";
-    private static String username = "SANTIAGO";
-    private static String password = "password";
+    private static final String URL = "jdbc:mysql://localhost:3306/jakarta_course";
+    private static final String USERNAME = "SANTIAGO";
+    private static final String PASSWORD = "password";
     private static Connection connection;
 
+    private DatabaseConnection() {}
+
     public static Connection getInstance() throws SQLException {
-        if (connection == null) {
-            connection = DriverManager.getConnection(url, username, password);
+        if (connection == null || connection.isClosed()) {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
             return connection;
         }
         return connection;
