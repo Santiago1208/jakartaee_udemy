9138cd09bff8fb375abece59382ee906291f67b0
Santiago Restrepo <santiago.restrepop@perficient.com>
2024-05-19 19:31:23 -0500

    Use the ProductDAO in the JdbcExample class
    Here we are not removing the connection object from the try/catch with resources because we want the resource closed when the block of code has ended. Depending on how you are architecting an application, you may want to keep the connection alive to use it across the application, or you may want to close it manually inside the DAO right after pulling out the data.

--- java_jdbc/src/main/java/org/srestrepo/java/jdbc/JdbcExample.java
+++ java_jdbc/src/main/java/org/srestrepo/java/jdbc/JdbcExample.java
@@ -1,27 +1,19 @@
 package org.srestrepo.java.jdbc;
 
+import org.srestrepo.java.jdbc.dao.GenericDAO;
+import org.srestrepo.java.jdbc.dao.ProductDAO;
+import org.srestrepo.java.jdbc.model.Product;
 import org.srestrepo.java.jdbc.util.DatabaseConnection;
 
 import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class JdbcExample {
 
     public static void main(String[] args) {
-        try (Connection connection = DatabaseConnection.getInstance();
-             Statement statement = connection.createStatement();
-             ResultSet result = statement.executeQuery("SELECT * FROM PRODUCTS")) {
-            while (result.next()) {
-                System.out.print(result.getInt("id"));
-                System.out.print(" | ");
-                System.out.print(result.getString("name"));
-                System.out.print(" | ");
-                System.out.print(result.getInt("price"));
-                System.out.print(" | ");
-                System.out.println(result.getDate("register_date"));
-            }
+        try (Connection connection = DatabaseConnection.getInstance()) {
+            GenericDAO<Product> productDAO = new ProductDAO();
+            productDAO.findAll().forEach(product -> System.out.println(product.getName()));
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
