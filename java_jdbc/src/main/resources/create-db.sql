815b2603371a6c21ef3a00ddd0866107f79488bb
Santiago Restrepo <santiago.restrepop@perficient.com>
2024-05-19 21:55:11 -0500

    Create the CATEGORIES table and binding to the PRODUCTS table

--- java_jdbc/src/main/resources/create-db.sql
+++ java_jdbc/src/main/resources/create-db.sql
@@ -1,7 +1,16 @@
+CREATE TABLE `categories` (
+    `id` int unsigned NOT NULL AUTO_INCREMENT,
+    `name` varchar(45) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
 CREATE TABLE `products` (
     `id` int unsigned NOT NULL AUTO_INCREMENT,
     `name` varchar(45) DEFAULT NULL,
     `price` int unsigned DEFAULT NULL,
     `register_date` datetime DEFAULT NULL,
-    PRIMARY KEY (`id`)
+    `category_id` int unsigned NOT NULL,
+    PRIMARY KEY (`id`),
+    CONSTRAINT `fk_category` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
