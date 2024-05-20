94d87e360599669f64a69ab1d05a616b7d99d69b
Santiago Restrepo <santiago.restrepop@perficient.com>
2024-05-19 23:48:56 -0500

    Update DatabaseConnection to set up the connection pool and the connection retrieval

--- java_jdbc/src/main/java/org/srestrepo/java/jdbc/util/DatabaseConnection.java
+++ java_jdbc/src/main/java/org/srestrepo/java/jdbc/util/DatabaseConnection.java
@@ -1,22 +1,41 @@
 package org.srestrepo.java.jdbc.util;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/jakarta_course";
     private static final String USERNAME = "SANTIAGO";
     private static final String PASSWORD = "password";
-    private static Connection connection;
+    private static BasicDataSource pool;
 
     private DatabaseConnection() {}
 
-    public static Connection getInstance() throws SQLException {
-        if (connection == null || connection.isClosed()) {
-            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-            return connection;
+    public static BasicDataSource getInstance() throws SQLException {
+        if (pool == null) {
+            pool = new BasicDataSource();
+            // Basic configuration
+            pool.setUrl(URL);
+            pool.setUsername(USERNAME);
+            pool.setPassword(PASSWORD);
+            // Initial capacity
+            pool.setInitialSize(3);
+            pool.setMinIdle(3);
+            pool.setMaxIdle(8);
+            pool.setMaxTotal(8);
         }
-        return connection;
+        return pool;
+    }
+
+    /**
+     * Gets one connection from the already configured pool.
+     * @return the connection object DAOs can use to do CRUD operations
+     * @throws SQLException if the pool could not get a connection because they all are busy
+     * or the pool is misconfigured.
+     */
+    public static Connection getConnection() throws SQLException {
+        return getInstance().getConnection();
     }
 }
