b636d8e7a1b5178a53d18a2b3ea5399b64852945
Santiago Restrepo <santiago.restrepop@perficient.com>
2024-06-07 16:02:24 -0500

    Turn off autocommit to manually commit and rollback JdbcExample

--- java_jdbc/src/main/java/org/srestrepo/java/jdbc/JdbcExample.java
+++ java_jdbc/src/main/java/org/srestrepo/java/jdbc/JdbcExample.java
@@ -4,38 +4,69 @@ import org.srestrepo.java.jdbc.dao.GenericDAO;
 import org.srestrepo.java.jdbc.dao.ProductDAO;
 import org.srestrepo.java.jdbc.model.Category;
 import org.srestrepo.java.jdbc.model.Product;
+import org.srestrepo.java.jdbc.util.DatabaseConnection;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Date;
 
 public class JdbcExample {
 
-    public static void main(String[] args) {
-        GenericDAO<Product> productDAO = new ProductDAO();
+    public static void main(String[] args) throws SQLException {
+        try (Connection connection = DatabaseConnection.getInstance().getConnection()) {
+            if (connection.getAutoCommit()) {
+                connection.setAutoCommit(false);
+            }
+            try {
+                GenericDAO<Product> productDAO = new ProductDAO();
 
-        // Find All
-        System.out.println("============ Find All ============");
-        productDAO.findAll().forEach(System.out::println);
+                // Find All
+                System.out.println("============ Find All ============");
+                productDAO.findAll().forEach(System.out::println);
 
-        // Find by ID
-        System.out.println("============ Find By ID ============");
-        System.out.println(productDAO.findById(2L));
+                // Find by ID
+                System.out.println("============ Find By ID ============");
+                System.out.println(productDAO.findById(2L));
 
-        // Save
-        System.out.println("============ Save Product ============");
-        Product newProduct = new Product();
-        newProduct.setName("Notebook Asus ROG");
-        newProduct.setPrice(2550);
-        newProduct.setRegisterDate(new Date());
+                // Save
+                System.out.println("============ Save Product ============");
+                Product newProduct = new Product();
+                newProduct.setName("IBM Keyboard");
+                newProduct.setPrice(1550);
+                newProduct.setRegisterDate(new Date());
+                newProduct.setSku("sku19");
 
-        Category technology = new Category();
-        technology.setId(3L);
-        newProduct.setCategory(technology);
+                Category technology = new Category();
+                technology.setId(3L);
+                newProduct.setCategory(technology);
 
-        productDAO.save(newProduct);
-        System.out.println("Product Saved successfully!");
+                productDAO.save(newProduct);
+                System.out.println("Product Saved successfully!");
 
-        // Find All - the saved product
-        System.out.println("============ Find All - the Saved Product ============");
-        productDAO.findAll().forEach(System.out::println);
+                // Update
+                System.out.println("============ Update Product ============");
+                newProduct = new Product();
+                newProduct.setId(20L);
+                newProduct.setName("Corsair K95 Keyboard");
+                newProduct.setPrice(700);
+                newProduct.setSku("sku20");
+
+                Category entertainment = new Category();
+                entertainment.setId(2L);
+                newProduct.setCategory(entertainment);
+
+                productDAO.save(newProduct);
+                System.out.println("Product Updated successfully!");
+
+                // Find All - the saved product
+                System.out.println("============ Find All Again ============");
+                productDAO.findAll().forEach(System.out::println);
+
+                connection.commit();
+            } catch (Exception e) {
+                e.printStackTrace();
+                connection.rollback();
+            }
+        }
     }
 }
