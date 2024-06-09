6b495b1026590dae8758e14b910e7a47e77f47ae
Santiago Restrepo <santiago.restrepop@perficient.com>
2024-06-09 10:31:18 -0500

    Validating the form in FormServlet

--- webapp-form/src/main/java/org/apiservlet/webapp/form/FormServlet.java
+++ webapp-form/src/main/java/org/apiservlet/webapp/form/FormServlet.java
@@ -7,7 +7,9 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Logger;
 
 @WebServlet("/register")
@@ -28,6 +30,29 @@ public class FormServlet extends HttpServlet {
         String enable = request.getParameter("enable");
         String secret = request.getParameter("secret");
 
+        List<String> errors = new ArrayList<>();
+        if (username == null || username.isBlank()) {
+            errors.add("Username is required");
+        }
+        if (password == null || password.isBlank()) {
+            errors.add("Password is required");
+        }
+        if (email == null || !email.contains("@")) {
+            errors.add("E-Mail is required and should be valid");
+        }
+        if (country == null || country.isBlank()) {
+            errors.add("Country is required");
+        }
+        if (programming == null || programming.length == 0) {
+            errors.add("At least one Programming Language is required");
+        }
+        if (roles == null || roles.length == 0) {
+            errors.add("At least one Role is required");
+        }
+        if (idiom == null) {
+            errors.add("Idiom is required");
+        }
+
         try (PrintWriter printWriter = response.getWriter()) {
             printWriter.println("<!DOCTYPE html>");
             printWriter.println("<html>");
@@ -38,19 +63,24 @@ public class FormServlet extends HttpServlet {
             printWriter.println("    <body>");
             printWriter.println("        <h1>Form Result</h1>");
             printWriter.println("        <ul>");
-            printWriter.println("           <li>Username: " + username + "</li>");
-            printWriter.println("           <li>Password: " + password + "</li>");
-            printWriter.println("           <li>E-Mail: " + email + "</li>");
-            printWriter.println("           <li>Country: " + country + "</li>");
-            printWriter.println("           <li>Programming Languages: <ul>");
-            Arrays.asList(programming).forEach(p -> printWriter.println("<li>" + p + "</li>"));
-            printWriter.println("           </ul></li>");
-            printWriter.println("           <li>Roles: <ul>");
-            Arrays.asList(roles).forEach(r -> printWriter.println("<li>" + r + "</li>"));
-            printWriter.println("           </ul></li>");
-            printWriter.println("           <li>Idiom: " + idiom + "</li>");
-            printWriter.println("           <li>Enable: " + enable + "</li>");
-            printWriter.println("           <li>Secret: " + secret + "</li>");
+            if (errors.isEmpty()) {
+                printWriter.println("           <li>Username: " + username + "</li>");
+                printWriter.println("           <li>Password: " + password + "</li>");
+                printWriter.println("           <li>E-Mail: " + email + "</li>");
+                printWriter.println("           <li>Country: " + country + "</li>");
+                printWriter.println("           <li>Programming Languages: <ul>");
+                Arrays.asList(programming).forEach(p -> printWriter.println("<li>" + p + "</li>"));
+                printWriter.println("           </ul></li>");
+                printWriter.println("           <li>Roles: <ul>");
+                Arrays.asList(roles).forEach(r -> printWriter.println("<li>" + r + "</li>"));
+                printWriter.println("           </ul></li>");
+                printWriter.println("           <li>Idiom: " + idiom + "</li>");
+                printWriter.println("           <li>Enable: " + enable + "</li>");
+                printWriter.println("           <li>Secret: " + secret + "</li>");
+            } else {
+                errors.forEach(error -> printWriter.println("<li>" + error + "</li>"));
+                printWriter.println("<p><a href=\"index.html\">Back</a></p>");
+            }
             printWriter.println("        </ul>");
             printWriter.println("    </body>");
             printWriter.println("</html>");
