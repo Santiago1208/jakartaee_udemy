3f750d30bb5a143c193862ef67a2dd4f3e47bae2
Santiago Restrepo <santiago.restrepop@perficient.com>
2024-06-09 21:07:14 -0500

    Prepare the project to export to XLS lesson

---
+++ webapp-headers/src/main/java/org/srestrepo/apiservlet/webapp/headers/services/ProductService.java
@@ -0,0 +1,10 @@
+package org.srestrepo.apiservlet.webapp.headers.services;
+
+import org.srestrepo.apiservlet.webapp.headers.models.Product;
+
+import java.util.List;
+
+public interface ProductService {
+    List<Product> getProducts();
+
+}
