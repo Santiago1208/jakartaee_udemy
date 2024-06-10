848afa4311f40a14a4694803d1b48e5dd8c3511a
Santiago Restrepo <santiago.restrepop@perficient.com>
2024-06-09 21:38:04 -0500

    Update ProductJSONServlet to allow receiving JSON inputs

--- webapp-headers/src/main/java/org/srestrepo/apiservlet/webapp/headers/controllers/ProductJSONServlet.java
+++ webapp-headers/src/main/java/org/srestrepo/apiservlet/webapp/headers/controllers/ProductJSONServlet.java
@@ -1,6 +1,7 @@
 package org.srestrepo.apiservlet.webapp.headers.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletInputStream;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -11,6 +12,7 @@ import org.srestrepo.apiservlet.webapp.headers.services.ProductService;
 import org.srestrepo.apiservlet.webapp.headers.services.ProductServiceImpl;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -30,6 +32,39 @@ public class ProductJSONServlet extends HttpServlet {
             response.getWriter().write(json);
         } catch (IOException e) {
             log.throwing(this.getClass().getName(), "doGet", e);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            ServletInputStream jsonStream = request.getInputStream();
+            ObjectMapper mapper = new ObjectMapper();
+            Product product = mapper.readValue(jsonStream, Product.class);
+            response.setContentType("text/html;charset=UTF-8");
+
+            try (PrintWriter printWriter = response.getWriter()) {
+                printWriter.println("<!DOCTYPE html>");
+                printWriter.println("<html>");
+                printWriter.println("    <head>");
+                printWriter.println("        <meta charset=\"UTF-8\">");
+                printWriter.println("        <title>Product Detail coming from JSON</title>");
+                printWriter.println("    </head>");
+                printWriter.println("    <body>");
+                printWriter.println("        <h1>Product Detail coming from JSON</h1>");
+                printWriter.println("        <ul>");
+                printWriter.println("        <li>" + product.getId() + "</li>");
+                printWriter.println("        <li>" + product.getName() + "</li>");
+                printWriter.println("        <li>" + product.getType() + "</li>");
+                printWriter.println("        <li>" + product.getPrice() + "</li>");
+                printWriter.println("        </ul>");
+                printWriter.println("    </body>");
+                printWriter.println("</html>");
+            } catch (IOException e) {
+                log.throwing(this.getClass().getName(), "doPost", e);
+            }
+        } catch (IOException e) {
+            log.throwing(this.getClass().getName(), "doPost", e);
         }
     }
 }
