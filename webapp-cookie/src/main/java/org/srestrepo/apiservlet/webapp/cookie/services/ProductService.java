2f2565cdb0b5c4f5dc488290aad7ba436ea008e5
Santiago Restrepo <sanpra1208@gmail.com>
2025-08-23 16:57:36 -0500

    Prepare project for Cookies lecture

---
+++ webapp-cookie/src/main/java/org/srestrepo/apiservlet/webapp/cookie/services/ProductService.java
@@ -0,0 +1,10 @@
+package org.srestrepo.apiservlet.webapp.cookie.services;
+
+import org.srestrepo.apiservlet.webapp.cookie.models.Product;
+
+import java.util.List;
+
+public interface ProductService {
+    List<Product> getProducts();
+
+}
---
+++ webapp-cookie/src/main/java/org/srestrepo/apiservlet/webapp/cookie/services/ProductServiceImpl.java
@@ -0,0 +1,15 @@
+package org.srestrepo.apiservlet.webapp.cookie.services;
+
+import org.srestrepo.apiservlet.webapp.cookie.models.Product;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ProductServiceImpl implements ProductService {
+    @Override
+    public List<Product> getProducts() {
+        return Arrays.asList(new Product(1L, "Notebook", "Computing", 175_000),
+                new Product(2L, "Desktop", "Office", 100_000),
+                new Product(3L, "Keyboard", "Computing", 40_000));
+    }
+}
