0bc158cbcdfd75159bd2310357033336567567d6
Santiago Restrepo <sanpra1208@gmail.com>
2025-08-30 19:55:42 -0500

    Create the Cart and CartItem classes

---
+++ webapp-session/src/main/java/org/srestrepo/apiservlet/webapp/session/models/Cart.java
@@ -0,0 +1,35 @@
+package org.srestrepo.apiservlet.webapp.session.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cart {
+    private final List<CartItem> cartItems;
+
+    public Cart() {
+        cartItems = new ArrayList<>();
+    }
+
+    public List<CartItem> getCartItems() {
+        return cartItems;
+    }
+
+    public void addItem(CartItem item) {
+        if (cartItems.contains(item)) {
+            cartItems.stream()
+                    .filter(i -> i.equals(item))
+                    .findFirst()
+                    .ifPresent(cartItem -> {
+                        cartItem.setAmount(cartItem.getAmount() + 1);
+                    });
+        } else {
+            cartItems.add(item);
+        }
+    }
+
+    public int getTotal() {
+        return cartItems.stream()
+                .mapToInt(CartItem::getLineTotal)
+                .sum();
+    }
+}
