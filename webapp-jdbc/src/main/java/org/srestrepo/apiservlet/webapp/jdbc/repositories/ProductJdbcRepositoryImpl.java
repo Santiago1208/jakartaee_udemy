99f3fea5341409c06912fc27fb1e19cb1d181b4e
Santiago Restrepo <sanpra1208@gmail.com>
2025-08-31 13:43:48 -0500

    Implement remaining CRUD methods in ProductJdbcRepositoryImpl

--- webapp-jdbc/src/main/java/org/srestrepo/apiservlet/webapp/jdbc/repositories/ProductJdbcRepositoryImpl.java
+++ webapp-jdbc/src/main/java/org/srestrepo/apiservlet/webapp/jdbc/repositories/ProductJdbcRepositoryImpl.java
@@ -43,12 +43,34 @@ public class ProductJdbcRepositoryImpl implements JdbcRepository<Product> {
 
     @Override
     public void save(Product entity) throws SQLException {
-
+        String sql;
+        boolean isUpdate = entity.getId() != null && entity.getId() > 0;
+        if (isUpdate) {
+            sql =  "update products set name = ?, price = ?, sku = ?, category_id = ? where id = ?";
+        } else {
+            sql = "insert into products (name, price, sku, category_id, created_at) values (?, ?, ?, ?, ?)";
+        }
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, entity.getName());
+            statement.setInt(2, entity.getPrice());
+            statement.setString(3, entity.getSku());
+            statement.setLong(4, entity.getCategory().getId());
+            if (isUpdate) {
+                statement.setLong(5, entity.getId());
+            } else {
+                statement.setDate(5, Date.valueOf(entity.getCreatedAt()));
+            }
+            statement.executeUpdate();
+        }
     }
 
     @Override
     public void delete(Long id) throws SQLException {
-
+        String sql = "delete from products where id = ?";
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setLong(1, id);
+            statement.executeUpdate();
+        }
     }
 
     private static Product getProduct(ResultSet resultSet) throws SQLException {
