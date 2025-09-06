086e3171208d66c76793511705b8f7b83490df79
Santiago Restrepo <sanpra1208@gmail.com>
2025-09-06 09:45:01 -0500

    Create User model and UserJdbcRepository

---
+++ webapp-auth-jdbc/src/main/java/org/srestrepo/apiservlet/webapp/authjdbc/repositories/UserJdbcRepository.java
@@ -0,0 +1,9 @@
+package org.srestrepo.apiservlet.webapp.authjdbc.repositories;
+
+import org.srestrepo.apiservlet.webapp.authjdbc.models.User;
+
+import java.sql.SQLException;
+
+public interface UserJdbcRepository extends JdbcRepository<User> {
+    User findByUsername(String username) throws SQLException;
+}
