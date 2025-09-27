a954afc1c12389aa077400847695fe5344d8104b
Santiago Restrepo <sanpra1208@gmail.com>
2025-09-27 18:05:51 -0500

    Simplify DataSource pull from web.xml with @Resource annotation

--- webapp-cdi/src/main/java/org/srestrepo/apiservlet/webapp/cdi/config/ResourceProducer.java
+++ webapp-cdi/src/main/java/org/srestrepo/apiservlet/webapp/cdi/config/ResourceProducer.java
@@ -1,18 +1,23 @@
 package org.srestrepo.apiservlet.webapp.cdi.config;
 
+import jakarta.annotation.Resource;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.enterprise.inject.Produces;
 import jakarta.inject.Named;
-import org.srestrepo.apiservlet.webapp.cdi.util.DatasourceJdbcConnection;
 
+import javax.sql.DataSource;
 import java.sql.Connection;
+import java.sql.SQLException;
 
 public class ResourceProducer {
+
+    @Resource(name = "jdbc/javaCourseDB")
+    private DataSource dataSource;
 
     @Produces
     @RequestScoped
     @Named("jdbcConnection")
-    private Connection produceJdbcConnection() {
-        return DatasourceJdbcConnection.getConnection();
+    private Connection produceJdbcConnection() throws SQLException {
+        return dataSource.getConnection();
     }
 }
