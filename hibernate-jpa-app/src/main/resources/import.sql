769212396ebacfd2400030fa7a3ba56b9c629ebb
Santiago Restrepo <sanpra1208@gmail.com>
2025-09-28 20:48:34 -0500

    Add automatic schema and data generation with drop-and-create and import.sql

---
+++ hibernate-jpa-app/src/main/resources/import.sql
@@ -0,0 +1,9 @@
+INSERT INTO public.clients ("name", surname, payment_type, created_at, updated_at) VALUES('Andres', 'Guzman', 'Debito', NULL, NULL);
+INSERT INTO public.clients ("name", surname, payment_type, created_at, updated_at) VALUES('John', 'Doe', 'Credito', NULL, NULL);
+INSERT INTO public.clients ("name", surname, payment_type, created_at, updated_at) VALUES('Pepa', 'Doe', 'Debito', NULL, NULL);
+INSERT INTO public.clients ("name", surname, payment_type, created_at, updated_at) VALUES('Luna', 'Garcia', 'Debito', NULL, NULL);
+INSERT INTO public.clients ("name", surname, payment_type, created_at, updated_at) VALUES('Lucy', 'Martinez', 'Credito', NULL, NULL);
+INSERT INTO public.clients ("name", surname, payment_type, created_at, updated_at) VALUES('John', 'Roe', 'PayPal', NULL, NULL);
+INSERT INTO public.clients ("name", surname, payment_type, created_at, updated_at) VALUES('Lou', 'Loe', 'PayPal', NULL, NULL);
+INSERT INTO public.clients ("name", surname, payment_type, created_at, updated_at) VALUES('Lalo', 'Mena', 'Webpay', '2025-09-28 20:10:05.658', '2025-09-28 20:11:49.617');
+INSERT INTO public.clients ("name", surname, payment_type, created_at, updated_at) VALUES('Pia', 'Perez', 'PayPal Plus', '2025-09-28 20:21:00.272', '2025-09-28 20:21:47.109');
